fcd2e65ff736ba570b550087f0a77f9caeef31fe
jieggii <jieggii@protonmail.com>
2024-01-05 22:51:34 +0200

    Reorder pin variables

--- include/pinout.h
+++ include/pinout.h
@@ -2,26 +2,26 @@
 
 #include "Arduino.h"
 
-/// Microphone pin:
-const unsigned short MICROPHONE_PIN = A1;
+/// Candle LED pin:
+const unsigned short CANDLE_LED_PIN = 5;
+
+/// Failure LED pin.
+const unsigned short FAILURE_LED_PIN = 9;
 
 /// Piezo buzzer pin:
 const unsigned short BUZZER_PIN = 4;
 
-/// Candle LED pin:
-const unsigned short CANDLE_LED_PIN = 5;
-
-/// 1602 LCD address:
+/// 16x2 LCD i2c address:
 const unsigned short LCD_ADDRESS = 0x27;
+
+/// Microphone pin:
+const unsigned short MICROPHONE_PIN = A1;
 
 /// Pin used for wake up interruption.
 const unsigned short WAKE_UP_INTERRUPT_PIN = 7;
 
 /// Debug button pin.
 const unsigned short DEBUG_BUTTON_PIN = 8;
-
-/// Failure LED pin.
-const unsigned short FAILURE_LED_PIN = 9;
 
 
 #define EVERLASTING_BIRTHDAY_CARD_PINOUT_H
