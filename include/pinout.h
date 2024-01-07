7dd93ac5408248585c20931a3f0a99359942fb2b
jieggii <jieggii@protonmail.com>
2024-01-07 22:28:46 +0200

    Set `MICROPHONE_PIN` to `A5`

--- include/pinout.h
+++ include/pinout.h
@@ -15,7 +15,8 @@ const unsigned short BUZZER_PIN = 4;
 const unsigned short LCD_ADDRESS = 0x27;
 
 /// Microphone pin:
-const unsigned short MICROPHONE_PIN = A1;
+//const unsigned short MICROPHONE_PIN = A1;
+const unsigned short MICROPHONE_PIN = A5;
 
 /// Pin used for wake up interruption.
 const unsigned short WAKE_UP_INTERRUPT_PIN = 7;
